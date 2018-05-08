d9dbf2d38f716eb1f1b3d0c41a6b8438a440c651
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2018-05-08 11:13:28 +0100

    fix missing audio file

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'dist/');
+const APP_DIR = path.resolve(__dirname, 'js/');
+
+const config = {
+  entry: APP_DIR + '/roll-a-die.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'roll-a-die.js',
+    publicPath: 'dist/',
+    library: 'rollADie'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?/,
+        include: APP_DIR,
+        loader: 'babel-loader'
+      },
+      { 
+        test: /\.css$/, 
+        loader: "style-loader!css-loader" 
+      },
+      {
+        test: /\.less$/,
+        loaders: ['style-loader', 'css-loader', 'less-loader']
+      },
+      { 
+        test: /\.(png|mp3)$/,
+        loader: "url-loader?limit=100000" 
+      },
+      { 
+        test: /\.jpg$/, 
+        loader: "file-loader" 
+      },
+      {
+        test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'file-loader'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      }
+    ]
+  }
+};
+
+module.exports = config;
