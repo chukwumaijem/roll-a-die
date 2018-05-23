40cc949468b022df2b37a914fe304523e353e5e6
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2018-05-23 09:45:12 +0100

    add test for invalid or missing params

--- __tests__/roll-a-die-test.js
+++ __tests__/roll-a-die-test.js
@@ -39,7 +39,7 @@ describe('Roll A Die', function () {
   });
 });
 
-describe('Verify Params', () => {
+describe('Check For Required Params', () => {
   afterEach(function () {
     Array.prototype.forEach.call(element.children, e => element.removeChild(e));
   });
@@ -56,8 +56,64 @@ describe('Verify Params', () => {
     expect(() => rollADie(options))
       .toThrow('Element to render dice animation not specified.');
   });
+  test('should throw on missing numberOfDice.', () => {
+    const options = Object.assign({}, defaultOption, { numberOfDice: null });
+    expect(() => rollADie(options)).toThrow('Number of dice to use not specified.');
+  });
+  test('should throw on missing callback function.', () => {
+    const options = Object.assign({}, defaultOption, { callback: null });
+    expect(() => rollADie(options))
+      .toThrow('Provide a callback function to recieve dice values.');
+  });
+});
+
+describe('Check For Invalid Params', () => {
+  afterEach(function () {
+    Array.prototype.forEach.call(element.children, e => element.removeChild(e));
+  });
+  const cb = (r) => result = r;
+  const defaultOption = {
+    element,
+    numberOfDice: 2,
+    callback: cb,
+    noSound: true,
+  };
+
   test('should throw on invalid element type.', () => {
     const options = Object.assign({}, defaultOption, { element: 'Hello World!' });
     expect(() => rollADie(options)).toThrow('"element" must be a HTMLElement');
   });
-})
+  test('should throw on invalid numberOfDice type.', () => {
+    let options = Object.assign({}, defaultOption, { numberOfDice: true });
+    expect(() => rollADie(options)).toThrow('"numberOfDice" must be a number.');
+    options = Object.assign({}, defaultOption, { numberOfDice: 2.9 });
+    expect(() => rollADie(options)).toThrow('"numberOfDice" must be an integer.');
+  });
+  test('should throw on invalid callback.', () => {
+    const options = Object.assign({}, defaultOption, { callback: 34 });
+    expect(() => rollADie(options)).toThrow('"callback" must be a function.');
+  });
+  test('should throw on invalid delay type.', () => {
+    const options = Object.assign({}, defaultOption, { delay: '5000' });
+    expect(() => rollADie(options))
+      .toThrow('Time is seconds. "delay" must be a number.');
+  });
+  test('should throw on invalid values type.', () => {
+    const options = Object.assign({}, defaultOption,
+      { values: { val: 'Hello World!' } });
+    expect(() => rollADie(options))
+      .toThrow('Values to generate. "values" must be an array of numbers.');
+  });
+  test('should throw on invalid values length.', () => {
+    const options = Object.assign({}, defaultOption,
+      { values: [4, 5, 6] });
+    expect(() => rollADie(options))
+      .toThrow('The length of "values" must be equal to the numberOfDice.');
+  });
+  test('should throw on invalid value in values type.', () => {
+    const options = Object.assign({}, defaultOption,
+      { values: [5, '6'] });
+    expect(() => rollADie(options))
+      .toThrow('6 in "values" must be a number.');
+  });
+});
